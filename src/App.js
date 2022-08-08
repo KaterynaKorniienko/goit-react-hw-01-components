adc37c669af120c13f0731f1c75ea5be12059180
KaterynaKorniienko <katerynakorniienko7@ukr.net>
2022-08-08 05:02:16 +0300

    react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import FriendList from "./components/FriendList";
+import Profile from "./components/Profile";
+import Statistics from "./components/Statistics";
+import TransactionHistory from "./components/TransactionHistory";
+
+import friends from "./data/friends.json";
+import user from "./data/user.json";
+import statisticalData from "./data/statistical-data.json";
+import transactions from "./data/transactions.json";
+
+import "./css/App.css"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={statisticalData} />
+      <Statistics stats={statisticalData}/>
+      <FriendList friends={friends}/>
+      <TransactionHistory items={transactions} />
+      
     </div>
   );
 }
